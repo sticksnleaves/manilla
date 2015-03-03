c31cdad8d68717e7a4dd33ee1f2f0d7715de9335
Anthony Smith <anthony@sticksnleaves.com>
2015-03-03 11:01:52 -0500

    Added unfolder comments.

--- lib/manilla.rb
+++ lib/manilla.rb
@@ -9,13 +9,11 @@ module Manilla
   #
   # @example Fold text on character
   #   Manilla.fold('Hello, world!', 8, "\r\n\s")
-  #
   #   => Hello, w
   #       orld!
   #
   # @example Fold text on word
   #   Manilla.fold('Hello, world!', 8, "\r\n\s", :word)
-  #
   #   => Hello,
   #       world!
   #
@@ -29,6 +27,23 @@ module Manilla
     Manilla::Folder.call(text, maxwidth, delimiter, break_on)
   end
 
+  #
+  # Moves from a folded delimited representation to a single line
+  # representation by treating delimiters as blank strings.
+  #
+  # @example Unfold text
+  #   text = "Hello, world!"
+  #
+  #   folded_text = Manilla.fold(text, 8, "\r\n", :word)
+  #   => Hello,
+  #       world!
+  #
+  #   Manilla.unfold(folded_text, "\r\n")
+  #   => Hello, world!
+  #
+  # @param text [String] Text to be unfolded
+  # @param delimiter [String] String used to denote line boundaries
+  #
   def self.unfold(text, delimiter)
     Manilla::Unfolder.call(text, delimiter)
   end
