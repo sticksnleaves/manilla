494d258612f1e09016a43f6497874c6dbd963cc6
Anthony Smith <anthony@sticksnleaves.com>
2015-03-02 22:15:29 -0500

    Added convenience #fold method to ::Manilla module

--- lib/manilla.rb
+++ lib/manilla.rb
@@ -1,5 +1,30 @@
+require 'manilla/folder'
+
 require 'manilla/version'
 
 module Manilla
-  # Your code goes here...
+  #
+  # Breaks long lines of text into multiple delimited representations.
+  #
+  # @example Fold text on character
+  #   Manilla.fold('Hello, world!', 8, "\r\n\s")
+  #
+  #   => Hello, w
+  #       orld!
+  #
+  # @example Fold text on word
+  #   Manilla.fold('Hello, world!', 8, "\r\n\s", :word)
+  #
+  #   => Hello,
+  #       world!
+  #
+  # @param text [String] The text to be folded
+  # @param maxwidth [Integer] The maximum length of a line of text
+  # @param delimiter [String] String used to denote line boundaries
+  # @param break_on [Symbol] Specifies whether to fold text on a character
+  #                          (`:char`) or word (`:word`) (default: `:char`)
+  #
+  def self.fold(text, maxwidth, delimiter, break_on = :char)
+    Manilla::Folder.call(text, maxwidth, delimiter, break_on)
+  end
 end
